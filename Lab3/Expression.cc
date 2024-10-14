bffde46a1c7eeb4df0bdc8a8830b647963ef2783
Barre Blomberg <fred03.blomberg@gmail.com>
2024-10-14 11:08:17 +0200

    Fixed small errors

--- Lab3/Expression.cc
+++ Lab3/Expression.cc
@@ -6,49 +6,49 @@
 #include <string>
 #include <cmath>
 
-Expression::Expression(std::string expression)
+Expression::Expression(std::string const &expression)
 {
-    std::stack<Node*> stack { };
-    std::istringstream iss { expression };
+    std::stack<Node *> stack{};
+    std::istringstream iss{expression};
     std::string element;
 
     while (iss >> element)
     {
         try
         {
-            double d std::stod(element);
+            double d{std::stod(element)};
 
-            // Check if element is integer or real 
+            // Check if element is integer or real
             if (std::fmod(d, 1.0) == 0.0)
             {
                 // Is integer
-                stack.push(new Integer { static_cast<int>(d) });
+                stack.push(new Integer{static_cast<int>(d)});
             }
-            else 
+            else
             {
                 // Is real
-                stack.push(new Real { d });
+                stack.push(new Real{d});
             }
         }
-        catch
+        catch (std::exception e)
         {
             if (element == "+")
             {
-                Node* first { stack.top() };
+                Node *first{stack.top()};
                 stack.pop();
-                Node* second { stack.top() };
+                Node *second{stack.top()};
                 stack.pop();
 
-                stack.push(new Addition { first, second });
+                stack.push(new Addition{first, second});
             }
             else if (element == "-")
             {
-                Node* first { stack.top() };
+                Node *first{stack.top()};
                 stack.pop();
-                Node* second { stack.top() };
+                Node *second{stack.top()};
                 stack.pop();
 
-                stack.push(new Subtraction { first, second });
+                stack.push(new Subtraction{first, second});
             }
         }
 
@@ -56,7 +56,7 @@ Expression::Expression(std::string expression)
     }
 }
 
-double Expression::evaluate()
+double Expression::evaluate() const
 {
     return root->evaluate();
 }
